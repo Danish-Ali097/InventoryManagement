4528cda638d2c24f27741d9a622516011cbaadd5
Danish Ali <50976119+Danish-Ali097@users.noreply.github.com>
2019-08-05 00:06:00 +0500

    added functionality

---
+++ InventoryManagement/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InventoryManagement.Models;
+
+namespace InventoryManagement.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public JsonResult ImageUpload(ImageUploadModel img)
+        {
+            var file = img.ImageFile;
+            if (file != null)
+            {
+                var filename = Path.GetFileName(file.FileName);
+                file.SaveAs(Server.MapPath(img.PathtoSave + filename));
+            }
+            return Json(file.FileName, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
